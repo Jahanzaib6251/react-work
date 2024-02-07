1610aac621a1b1a07d554b2b18060de200fad154
jahanzaib6251 <jahanzaib.6251@gmail.com>
2024-02-07 14:09:15 +0500

    first commit 2

---
+++ weather-forecast/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import WeatherData from './Components/WeatherData';
+
+function App() {
+  return (
+    <div className='bg-container'>
+      <WeatherData></WeatherData>
+    </div>
+  );
+}
+
+export default App;
---
+++ weather-forecast/src/Components/WeatherData.jsx
@@ -0,0 +1,55 @@
+// orignal 
+import '../App.css';
+import React, { useEffect, useState } from 'react';
+
+const WeatherData = () => {
+  const [city, setCity] = useState('');
+  const [isLoading, setIsLoading] = useState(false)
+  const [myData, setMyData] = useState({})
+  
+  const getMyPostData =async ()=>{
+    setIsLoading(true);
+    try {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d222781c4065264867116c8e79592c53`)
+      const data =await response.json()
+      setMyData(data)
+      setIsLoading(false)
+    } 
+    catch (error) {
+      console.error(error)
+    } 
+  };
+
+  useEffect(() => {
+    console.log(myData);
+  }, [myData])
+  
+  return (
+    <div className="weather-data">
+      <div>
+        <input
+          type="text"
+          defaultValue={city}
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+        />
+        <button onClick={getMyPostData}>Get Weather Info</button>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : myData.name ? (
+          <div>
+            <h2>City: {myData.name}</h2>
+            <p>Temperature: {myData.main.temp} K</p>
+            <p>Humidity: {myData.main.humidity}%</p>
+            <p>Wind Speed: {myData.wind.speed} m/s</p>
+          </div>
+        ) : (
+          <p>Data is not Available.</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default WeatherData
